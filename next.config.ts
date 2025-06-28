f918488059719143a90156615a0b11d7cfa98b23
dnsxmrs <ericemarial@gmail.com>
2025-06-28 23:29:51 +0800

    feat: Integrate Prisma plugin and update configuration files

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,28 @@
 import type { NextConfig } from "next";
+import { PrismaPlugin } from "@prisma/nextjs-monorepo-workaround-plugin";
 
+/** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'img.youtube.com',
-        port: '',
-        pathname: '/vi/**',
-      },
-    ],
-  },
+    devIndicators: false,
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'img.youtube.com',
+                port: '',
+                pathname: '/vi/**',
+            },
+        ],
+    },
+    experimental: {
+        authInterrupts: true,
+    },
+    webpack: (config, { isServer }) => {
+        if (isServer) {
+            config.plugins = [...config.plugins, new PrismaPlugin()];
+        }
+        return config;
+    },
 };
 
 export default nextConfig;
