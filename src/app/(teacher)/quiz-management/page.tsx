7d79ca73748d2bd53c2c725cd691d2fa6dcb1f67
dnsxmrs <ericemarial@gmail.com>
2025-07-31 10:00:50 +0800

    refactor: Remove unused SVG files and QuizManagement component

--- src/app/(teacher)/quiz-management/page.tsx
+++
@@ -1,9 +0,0 @@
-import QuizManagement from '@/components/QuizManagement'
-
-export default function QuizManagementPage() {
-    return (
-        <div className="container mx-auto px-4 py-8">
-            <QuizManagement />
-        </div>
-    )
-}
